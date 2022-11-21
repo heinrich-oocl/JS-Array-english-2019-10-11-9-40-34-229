6ec0703f46a70bcdfd00ae956de7a20d2d17bf3f
heinrich-oocl <117629815+heinrich-oocl@users.noreply.github.com>
2022-11-21 13:18:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
+console.log(Array.isArray(a))
+console.log(Array.isArray(b))
 
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
+a = a.map((i) => i*2)
 
 
 // Write the program, according to the following requirements output results.
@@ -14,13 +17,45 @@ var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
+output = ""
+expr = 1
+switch(expr){
+    case 1:
+        output = colors.join(" ")
+        break
+    case 2:
+        output = colors.join("+")
+        break
+    default:
+        output = colors.join(",")
+}
+console.log(output)
 
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
-
+a.sort((a,b) => a>b).reverse()
+console.log(a)
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+let dict = {};
+let maxValue = 0;
+let maxCount = 0;
+
+
+for (let i of a) {
+  if (dict[i] == undefined) {
+    dict[i] = 1;
+  } else {
+    dict[i]++;
+  }
+
+  if (dict[i] > maxCount) {
+    maxCount = dict[i];
+    maxValue = i;
+  }
+}
+console.log(maxValue)
